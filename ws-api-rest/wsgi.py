61da400ce7a3ed97c2428caa5cec29b168f062f0
Pau Agustí Fernández <paupaf3@github.io>
2020-12-31 12:25:08 +0100

    Minor changes

--- ws-api-rest/wsgi.py
+++ ws-api-rest/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for WsApiRest project.
+WSGI config for ws-api-rest project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WsApiRest.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ws-api-rest.settings')
 
 application = get_wsgi_application()
