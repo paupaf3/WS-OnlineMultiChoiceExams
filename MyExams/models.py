073e5779040716f629a9cc7688919fbe3b1aa4f7
Pau Agustí Fernández <paupaf3@github.io>
2021-01-07 12:21:44 +0100

    Description Input and Output of the methods explained

--- MyExams/models.py
+++ MyExams/models.py
@@ -7,7 +7,6 @@ class Exam(models.Model):
     date=models.DateField()
     location=models.CharField(max_length=40)
     exam_file=models.FileField(upload_to="exam_files/", unique=True)
-    #exam_file=models.FileField(default='example_files/exam_files/noexam.csv')
 
 class User(models.Model):
     STUDENT = 'student'
@@ -23,6 +22,6 @@ class Grade(models.Model):
     exam=models.ForeignKey(Exam, on_delete=models.PROTECT)
     user=models.ForeignKey(User, on_delete=models.CASCADE)
     grade_file=models.FileField(upload_to="grade_files/")
-    #grade_file=models.FileField(default='example_files/exam_files/nograde.csv')
+
     class Meta:
         unique_together = ('exam', 'user',)
--- MyExams/views.py
+++ MyExams/views.py
@@ -7,15 +7,18 @@ from django.http import HttpResponse
 from .models import Exam, User, Grade
 from .serializers import ExamSerializer, GradeSerializer, UserSerializer
 
-'''
-#####################################################################################
-                                    EXAM
-#####################################################################################
-'''
 
-#GET d'un exam en concret per ID (pk=primary key)
+#####################################################################################
+                                    #EXAM
+#####################################################################################
+
 @api_view(['GET', ])
 def exam_view(request, pk):
+    """
+    Description: Shows an exam
+    Input: Exam ID(pk)
+    Output: Exam json
+    """
     try:
         exam = Exam.objects.get(pk=pk)
     except Exam.DoesNotExist:
@@ -25,9 +28,13 @@ def exam_view(request, pk):
         serializer = ExamSerializer(exam)
         return Response(serializer.data)
 
-#GET descarga d'un exam en concret per ID (pk=primary key)
 @api_view(['GET', ])
 def exam_download_view(request, pk):
+    """
+    Description: Downloads an exam
+    Input: Exam ID(pk)
+    Output: Exam csv file
+    """
     try:
         exam = Exam.objects.get(pk=pk)
     except Exam.DoesNotExist:
@@ -40,9 +47,12 @@ def exam_download_view(request, pk):
             response['Content-Disposition'] = 'attachment; filename=exam_file.csv'
             return response
 
-#GET de la llista amb tots els examens
 @api_view(['GET', ])
 def exam_list_view(request):
+    """
+    Description: Shows all the exams
+    Output: List of exams json
+    """
     try:
         examList = Exam.objects.all()
     except Exam.DoesNotExist:
@@ -52,10 +62,13 @@ def exam_list_view(request):
         serializer = ExamSerializer(examList, many=True)
         return Response(serializer.data)
 
-#GET de la busqueda d'un exam per descripcio
-#busca que la descripcio contingui la busqueda realitzada (description__contains)
 @api_view(['GET', ])
 def exam_search_view(request):
+    """
+    Description: Shows list of exams that the description contains the search
+    Input: Keyword of the search
+    Output: List of exams json
+    """
     try:
         examList = Exam.objects.filter(description__contains=request.GET['description'])
     except Exam.DoesNotExist:
@@ -65,28 +78,33 @@ def exam_search_view(request):
         serializer = ExamSerializer(examList, many=True)
         return Response(serializer.data)
 
-#PUT modificar la descripcio d'un examen en concret per ID
 @api_view(['PATCH', ])
 def exam_update_view(request, pk):
+    """
+    Description: Modify description of an exam
+    Input: Data in the request(the description) and Exam ID(pk)
+    Output: Exam json
+    """
     try:
         exam = Exam.objects.get(pk=pk)
     except Exam.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == "PATCH":
-        #Partial per permetre modificar sol la descripcio de l'exam
         serializer = ExamSerializer(exam, data=request.data, partial=True)
         data = {}
-        #Per comprovar que compleix amb la forma
         if serializer.is_valid():
             serializer.save()
             data["success"] = "Update Successful!"
             return Response(data=data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#DELETE eliminar exam per ID
 @api_view(['DELETE', ])
 def exam_delete_view(request, pk):
+    """
+    Description: Deletes an exam
+    Input: Exam ID(pk)
+    """
     try:
         exam = Exam.objects.get(pk=pk)
     except Exam.DoesNotExist:
@@ -101,9 +119,13 @@ def exam_delete_view(request, pk):
             data["failure"] = "Delete Failed! Exam has grades!"
         return Response(data=data)
 
-#POST crear exam
 @api_view(['POST', ])
 def exam_create_view(request):
+    """
+    Description: Create an exam
+    Input: Data necessary to create the exam
+    Output: Exam json
+    """
     exam = Exam()
     if request.method == "POST":
         serializer = ExamSerializer(exam, data=request.data)
@@ -112,15 +134,18 @@ def exam_create_view(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)   
 
-'''
-#####################################################################################
-                                    GRADE
-#####################################################################################
-'''
 
-#GET veure nota per id d'examen i id d'usuari 
+#####################################################################################
+                                    #GRADE
+#####################################################################################
+
 @api_view(['GET', ])
 def grade_view(request, exam, user):
+    """
+    Description: Shows grade using json
+    Input: Exam ID(exam) and User ID(user)
+    Output: Grade json
+    """
     try:
         grade = Grade.objects.get(exam=exam, user=user)
     except Grade.DoesNotExist:
@@ -130,9 +155,13 @@ def grade_view(request, exam, user):
         serializer = GradeSerializer(grade)
         return Response(serializer.data)
 
-#GET descargar nota per id d'examen i id d'usuari 
 @api_view(['GET', ])
 def grade_download_view(request, exam, user):
+    """
+    Description: Download the grade file of a grade
+    Input: Exam ID(exam) and User ID(user)
+    Output: Grade csv file
+    """
     try:
         grade = Grade.objects.get(exam=exam, user=user)
     except Grade.DoesNotExist:
@@ -145,9 +174,12 @@ def grade_download_view(request, exam, user):
             response['Content-Disposition'] = 'attachment; filename=grade_file.csv'
             return response
 
-#GET de la llista amb totes les notes
 @api_view(['GET', ])
 def grade_list_view(request):
+    """
+    Description: Shows a list of all the grades
+    Output: List of grades json
+    """
     try:
         gradeList = Grade.objects.all()
     except Grade.DoesNotExist:
@@ -157,9 +189,13 @@ def grade_list_view(request):
         serializer = GradeSerializer(gradeList, many=True)
         return Response(serializer.data)
 
-#GET de la busqueda de notes per id d'usuari
 @api_view(['GET', ])
 def grade_user_view(request, user):
+    """
+    Description: Shows a list of all the grades a particular User has
+    Input: User ID (user)
+    Output: List of grades json
+    """
     try:
         gradeList = Grade.objects.filter(user=user)
     except Grade.DoesNotExist:
@@ -169,10 +205,12 @@ def grade_user_view(request, user):
         serializer = GradeSerializer(gradeList, many=True)
         return Response(serializer.data)
 
-
-#DELETE eliminar nota per examID i userID
 @api_view(['DELETE', ])
 def grade_delete_view(request, exam, user):
+    """
+    Description: Deletes a grade
+    Input: Exam ID(exam) and User ID(user)
+    """
     try:
         grade = Grade.objects.get(exam=exam, user=user)
     except Grade.DoesNotExist:
@@ -187,9 +225,13 @@ def grade_delete_view(request, exam, user):
             data["failure"] = "Delete Failed!"
         return Response(data=data)
 
-#POST crear notes
 @api_view(['POST', ])
 def grade_create_view(request):
+    """
+    Description: Create a grade
+    Input: Data necessary to create the grade
+    Output: Grade json
+    """
     grade = Grade()
     if request.method == "POST":
         serializer = GradeSerializer(grade, data=request.data)
